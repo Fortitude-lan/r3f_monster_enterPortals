a042c28121c9b18ba8c05383490faa4d0d400c0b
Fortitude-lan <65820434+Fortitude-lan@users.noreply.github.com>
2023-12-24 11:29:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5678,
-  },
+  base: "/r3f_monster_enterPortals/"
 });
